62f57fce1614c175cc792434af13d5fc3831ec31
nob <>
2018-06-09 12:14:48 +0900

    [update]TodoAppの分割

--- src/index.js
+++ src/index.js
@@ -1,83 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
-import './index.css';
-import App from './App';
+import tasksReducer from './reducers/tasks';
+import TodoApp from './components/TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 
-const initialState = {
-  task: '',
-  tasks: []
-};
-
-function tasksReducer(state = initialState, action) {
-  switch(action.type) {
-    case 'INPUT_TASK':
-      return {
-        ...state,
-        task: action.payload.task
-      };
-    case 'ADD_TASK':
-      return {
-        ...state,
-        tasks: state.tasks.concat([action.payload.task])
-      };
-    default:
-      return state;
-  }
-}
-
-function resetReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'RESET_TASK' :
-      return {
-        ...state,
-        tasks: []
-      };
-    default :
-      return state;
-  }
-}
-
-
 const store = createStore(tasksReducer);
-
-const addTask = (task) => ({
-  type:'ADD_TASK',
-  payload: {
-    task
-  }
-});
-const inputTask = (task) => ({
-  type:'INPUT_TASK',
-  payload: {
-    task
-  }
-});
-
-const resetTask = () => ({
-  type: 'RESET_TASK'
-});
-
-function TodoApp({ store }) {
-  const {task, tasks} = store.getState();
-  return (
-    <div>
-      <input type="text" onChange={(e) => store.dispatch(inputTask(e.target.value))} />
-      <input type="button" value="add" onClick={() => store.dispatch(addTask(task))} />
-      <ul>
-        {
-          tasks.map(function(item,i) {
-            return (
-              <li key={i}>{item}</li>
-            );
-          })
-        }
-      </ul>
-    </div>
-  )
-}
 
 function renderApp() {
   render(
